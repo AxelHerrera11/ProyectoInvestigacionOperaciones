b1e69e63ac50785010f8588a2fd5554ee2950bc8
AxelHerrera11 <aherreras@miumg.edu.gt>
2025-09-10 20:52:54 -0600

    Actualización en ControladorMetodoGrafico

--- src/controlador/ControladorMetodoGrafico.java
+++ src/controlador/ControladorMetodoGrafico.java
@@ -6,17 +6,13 @@ package controlador;
 
 import implementacion.MetodoGraficoImp;
 import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Font;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.JTextArea;
 import javax.swing.SwingConstants;
 import javax.swing.table.DefaultTableModel;
 import modelo.ModeloMetodoGrafico;
